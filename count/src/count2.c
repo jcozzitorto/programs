c6f51a332585180aedbc363113d28e04c285460a
jcozzitorto <jcozzitorto@gmail.com>
2020-07-18 18:02:07 -0400

    'date'

--- count/src/count2.c
+++ count/src/count2.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define IN 	1
 #define OUT 	0
 
-int main(int argc, char *argv[]) {
-	int c, nc, nl, nw, state;
+int main(int argc, char **argv) {
+	int nc, nl, nw, state;
+	char c;
 
-	FILE *fp;
-
-	fp = fopen(argv[1], "r");
+	FILE *fp = fopen(argv[1], "r");
 	
-	state = OUT;
+	if ((fp == NULL) || (argc < 2)) {
+		printf("Invalid file\n");
+		exit(0);
+	}
+
+	/* state = OUT;
 	nc = nl = nw = 0;
 	while ((c = getchar()) != EOF) {
 		++nc;
@@ -25,9 +30,13 @@ int main(int argc, char *argv[]) {
 			state = IN;
 			++nw;
 		}
-
-	}
+	
+		
+	} */
 	
 	printf("%d, %d, %d\n", nc, nw, nl);
+
+	fclose(fp);
+	return(0);
 }
 	
