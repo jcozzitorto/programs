b8579e22e25e86ffb713e316d6a715251ae542f7
jcozzitorto <jcozzitorto@gmail.com>
2020-07-19 12:04:57 -0400

    Sun 19 Jul 2020 12:05:03 PM EDT

--- copy/src/copy3.c
+++ copy/src/copy3.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv) {
 	
 	FILE *fpi = fopen(argv[1], "r");
-	FILE *fpo = fopen(argv[2], "r+");
+	FILE *fpo = fopen(argv[2], "w");
 
 	if ((argc < 2) || (fpi == NULL) || (fpo == NULL)) {
-		return 1;
+		exit(1);
+	}
+		
+	char c = fgetc(fpi);
+
+	while (c != EOF) {
+
+		fputc(c, fpo);
+		c = fgetc(fpi);
 	}
 
 }
